631eacd83acdfcd6b365bdb016d4b23118818ca0
Roman Melnyk <rmelnik7777@gmail.com>
2023-03-31 10:57:00 +0300

    update

--- HTML-Editor-TinyMCE/ViewController.swift
+++ HTML-Editor-TinyMCE/ViewController.swift
@@ -18,7 +18,42 @@ class ViewController: UIViewController {
             webView.load(request)
         }
     }
+    
+    public func runJS(_ js: String, handler: ((String) -> Void)? = nil) {
+        webView.evaluateJavaScript(js) {(result, error) in
+            if let error = error {
+                print("WKWebViewJavascriptBridge Error: \(String(describing: error)) - JS: \(js)")
+                handler?("")
+                return
+            }
+            
+            guard let handler = handler else { return }
+            if let resultStr = result as? String {
+                handler(resultStr)
+                return
+            }
+            handler("") // no result
+        }
+    }
 
-
+    @IBAction func getHtmlTap(_ sender: Any) {
+        runJS("tinymce.activeEditor.getContent();", handler: {[weak self] html in
+            self?.showAlertView(html)
+        })
+    }
+    
 }
 
+extension UIViewController {
+    func showAlertView(_ title: String?, _ description: String? = nil) {
+        DispatchQueue.main.async {
+            let alertController = UIAlertController(title: title, message: description, preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Oк", style: .default, handler: {[weak self] _ in
+                if title == "Page not found" {
+                    self?.navigationController?.popViewController(animated: true)
+                }
+            }))
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+}
